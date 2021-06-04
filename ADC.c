d9e3f3ae65c8e92f11f10032a980733be8ea8673
Mahmoud-Ayoub <84600209+Mahmoud-Ayoub@users.noreply.github.com>
2021-06-04 15:02:39 +0200

    Add files

---
+++ ADC.c
@@ -0,0 +1,29 @@
+﻿/*
+ * ADC_DRIVER.c
+ *
+ * Created: 23/07/2020 04:14:44 م
+ *  Author: Blu-Ray
+ */ 
+#include "ADC_DRIVER.h"
+
+void ADC_init (void) {
+	//SET_OUTPUT_BIT(ADMUX,6) ;          // AREF = AVCC
+	
+	//RESET_INPUT_BIT(ADMUX,0) ;         // ADC0
+	//RESET_INPUT_BIT(ADMUX,1) ;
+	//RESET_INPUT_BIT(ADMUX,2) ;
+	//RESET_INPUT_BIT(ADMUX,3) ;
+	//RESET_INPUT_BIT(ADMUX,4) ;
+	ADMUX = 0xC0 ; 
+	//SET_OUTPUT_BIT(ADCSRA,7) ;           // ADC enable
+	//SET_OUTPUT_BIT(ADCSRA,0) ;           // prescaler = 128
+	//SET_OUTPUT_BIT(ADCSRA,1) ;
+	//SET_OUTPUT_BIT(ADCSRA,2) ;
+	 ADCSRA = 0x87 ; 
+	
+}
+uint16_t ADC_read (void) {
+	SET_OUTPUT_BIT(ADCSRA,6) ;        // start conversion
+	while (ADCSRA & (1<<ADIF) == 0);   // check if conversion is completed
+	return ADC ;
+}
---
+++ ADC.h
@@ -0,0 +1,22 @@
+﻿/*
+ * ADC_DRIVER.h
+ *
+ * Created: 23/07/2020 04:13:22 م
+ *  Author: Blu-Ray
+ */ 
+
+
+#ifndef ADC_DRIVER_H_
+#define ADC_DRIVER_H_
+
+#include <avr/io.h>
+#include "GPIO.h"
+#include <util/delay.h>
+
+void ADC_init (void)  ;
+uint16_t ADC_read (void) ;
+
+
+
+
+#endif /* ADC_DRIVER_H_ */
